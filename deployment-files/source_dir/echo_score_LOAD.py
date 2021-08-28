423e6aae91dafe4ae28e16b7cef8b452a6711a8f
rl16432 <rl16432@my.westlake.school.nz>
2021-08-28 12:06:03 +1200

    Updated registered model framework, and cleaned up deployment notebooks and score scripts

--- deployment-files/source_dir/echo_score_LOAD.py
+++ deployment-files/source_dir/echo_score_LOAD.py
@@ -1,10 +1,7 @@
-from functools import total_ordering
 import json
 import tensorflow as tf
-import random
 import os 
 import numpy as np
-import pandas as pd
 from azureml.core import Workspace, Dataset
 
 def init():
@@ -14,20 +11,9 @@ def init():
   model_root = os.getenv('AZUREML_MODEL_DIR')
   tf_model_folder = 'model1'
 
-  # Read training and test sets to calculate maximum length or the amount of padding required
-  dataset_train = pd.read_csv(os.path.join(model_root, tf_model_folder, "assets/train.csv"))
-  dataset_test = pd.read_csv(os.path.join(model_root, tf_model_folder, "assets/test.csv"))
-  
-  # Extract excerpts from each dataset into Numpy array from
-  train_text = dataset_train.loc[:,'excerpt'].values
-  pred_text = dataset_test.loc[:,'excerpt'].values
-  
-  # Combine text into one array and create word sequences for each text sample
-  total_text = np.concatenate(train_text, pred_text)
-  total_text_sequences = np.array([tf.keras.preprocessing.text.text_to_word_sequence(text) for text in total_text], dtype = "object")
-  
-  # Calculate maximum length
-  max_length = max([len(sequence) for sequence in total_text_sequences])
+  # Load maximum length from max_length.txt
+  with open(os.path.join(model_root, tf_model_folder, 'assets/max_length.txt')) as text:
+    max_length = int(text.read())
   
   # Load tokenizer from the saved tokenizer
   with open(os.path.join(model_root, tf_model_folder, 'assets/tokenizer.json')) as f:
