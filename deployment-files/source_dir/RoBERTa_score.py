c01229f66dc04be968d9486e275979684b029141
rl16432 <rl16432@my.westlake.school.nz>
2021-08-30 21:45:56 +1200

    Added score script for RoBERTa and setup RoBERTa deployment notebook

---
+++ deployment-files/source_dir/RoBERTa_score.py
@@ -0,0 +1,44 @@
+import os
+import json
+from transformers import RobertaTokenizerFast, AutoModelForSequenceClassification
+
+MAX_LENGTH = 256
+
+def init():
+    global tokenizer, model
+
+    # Initialise model directories
+    model_dir = os.getenv("AZUREML_MODEL_DIR")
+    model_folder = "Commonlit-RoBERTa-Base"
+
+    # Initialise RoBERTa tokenizer
+    tokenizer = RobertaTokenizerFast.from_pretrained(os.path.join(model_dir, model_folder, "tokenizer"))
+
+    # Initialise pretrained RoBERTa-Base model
+    model = AutoModelForSequenceClassification.from_pretrained(os.path.join(model_dir, model_folder))
+
+def run(request):
+    global tokenizer, model
+
+    data = json.loads(request)
+
+    # Load data
+    inputs = list(data["data"])
+
+    # Tokenize data using RoBERTa tokenizer
+    token_seqs = tokenizer(inputs, padding = "max_length", 
+                            max_length = MAX_LENGTH, 
+                            truncation = True, 
+                            return_tensors = "pt")
+    
+    # Pass through pre-trained model
+    outputs_tensor = model(**token_seqs)
+
+    # Convert model outputs to list
+    outputs = outputs_tensor.logits.flatten().tolist()
+    
+    # Display targets
+    print("Results: " + str(outputs))
+
+    return outputs
+
